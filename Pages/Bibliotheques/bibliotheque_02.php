a9f4349b87b1b519bdf81c341a81ca15905a4381
Bastien <chevalierbastien770@gmail.com>
2024-04-08 14:15:54 +0200

    navbar fix

--- Pages/Bibliotheques/bibliotheque_02.php
+++ Pages/Bibliotheques/bibliotheque_02.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../CSS/css_bibliotheque.css">
     <title>Document</title>
+    <link rel="stylesheet" href="../../CSS/css_bibliotheque.css">
 </head>
 <body>
     <?php
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,10 @@
 <nav>
-    <a href="index.php">Menu principal</a>
-    <a href="Pages/Bibliotheques/bibliotheque_01.php">Gestion des bibliothèques</a>
-    <a href="Pages/Auteurs/auteur_01.php">Gestion des auteurs</a>
-    <a href="Pages/Ouvrages/ouvrages_01.php">Gestion des ouvrages</a>
-    <a href="Pages/Usagers/usagers_01.php">Gestion des usagers</a>
-    <a href="emprunt_01.php">Gestion des emprunts</a>
-    <a href="Pages/retour_01.php">Gestion des retours</a>
-    <a href="Pages/Transferts/transfert_01.php">Gestion des transferts</a>
+    <a href="../../index.php">Menu principal</a>
+    <a href="../Bibliotheques/bibliotheque_01.php">Gestion des bibliothèques</a>
+    <a href="../Auteurs/auteur_01.php">Gestion des auteurs</a>
+    <a href="../Ouvrages/ouvrages_01.php">Gestion des ouvrages</a>
+    <a href="../Usagers/usagers_01.php">Gestion des usagers</a>
+    <a href="../emprunt_01.php">Gestion des emprunts</a>
+    <a href="../Retours/retour_01.php">Gestion des retours</a>
+    <a href="../Transferts/transfert_01.php">Gestion des transferts</a>
 </nav>
