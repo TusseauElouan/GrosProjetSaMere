0956b73ca027d8a9e480cabd1a28996d2c197af1
AryaMerlet <arya.merlet@gmail.com>
2024-04-08 13:56:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
         <a href="Pages/Auteurs/auteur_01.php">Gestion des auteurs</a>
         <a href="Pages/Ouvrages/ouvrages_01.php">Gestion des ouvrages</a>
         <a href="Pages/Usagers/usagers_01.php">Gestion des usagers</a>
-        <a href="emprunt_01.php">Gestion des emprunts</a>
-        <a href="Pages/retour_01.php">Gestion des retours</a>
+        <a href="Pages/Emprunts/emprunt_01.php">Gestion des emprunts</a>
+        <a href="Pages/Retour/retour_01.php">Gestion des retours</a>
         <a href="Pages/Transferts/transfert_01.php">Gestion des transferts</a>
     </nav>
 
