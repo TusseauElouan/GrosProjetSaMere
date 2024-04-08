285c97d7ad9f57e1fed083fb214fc58bee265c20
Félis Maillard <151616377+FelisMaillard@users.noreply.github.com>
2024-04-08 14:02:48 +0200

    EMPRUNT

---
+++ Pages/Emprunt/emprunt_01.php
@@ -0,0 +1,72 @@
+<?php
+$host = 'localhost';
+$db = 'bibliotheque';
+$user = 'root';
+$pass = '';
+$port = 3306;
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+$pdo = new PDO($dsn, $user, $pass);
+?>
+<!doctype html>
+<html lang="fr">
+    <head>
+        <title>Projet - Biblio</title>
+        <meta charset="utf-8" />
+        <meta name="viewport"content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <link rel="stylesheet" href="../../CSS/css_bibliotheque.css">
+    </head>
+
+    <body>
+        <header>
+            <?php
+            // include("includes/header.php");
+            ?>
+        </header>
+        <main>
+            <div>
+                <table border="1px">
+                <tr><th>Nom</th><th>Prénom</th><th>Commentaire</th><th>Modifs</th></tr>
+                <?php
+                    $sql = 'SELECT * FROM auteur';
+                    $temp = $pdo->query($sql);
+                    while ($auteur = $temp->fetch()) {
+                        $id = $auteur['numero_auteur'];
+                        $nom = $auteur['nom_auteur'];
+                        $prenom = $auteur['prenom_auteur'];
+                        $commentaire = $auteur['commentaire']; 
+                ?>
+                        <tr>
+                        <td><?= $nom ?></td>
+                        <td><?= $prenom ?></td>
+                        <td><?= $commentaire ?></td>
+                        <td>
+                            <a href="auteur_02.php?id=<?= $id ?>&nom=<?= $nom ?>&prenom=<?= $prenom ?>&commentaire=<?= $commentaire ?>">
+                                <img src="../../Medias/editform.png" class="boutonsform" alt="image de modification">
+                            </a>
+                            <a href="auteur_03.php?id=<?= $id ?>">
+                                <img src="../../Medias/supprimerform.png" class="boutonsform" alt="">
+                            </a>
+                        </td>
+                        </tr>
+                <?php
+                    }
+                ?>
+                </table>
+                <br />
+                <a href="auteur_04.php">
+                    <img src="../../Medias/ajouterform.png" class="boutonsform" alt="">
+                    Add
+                </a>
+            </div>
+
+
+        </main>
+        <footer>
+            <?php
+            // include("includes/footer.php");
+            ?>
+        </footer>
+    </body>
+</html>
---
+++ Pages/Emprunt/emprunt_04.php
@@ -0,0 +1,82 @@
+<?php
+$host = 'localhost';
+$db = 'bibliotheque';
+$user = 'root';
+$pass = '';
+$port = 3306;
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+$pdo = new PDO($dsn, $user, $pass);
+
+// verification de l'existance des valeures dans le liens (si ca existe je prepare les variables pour mon form)
+if (isset($_REQUEST['id'],$_REQUEST['nom'],$_REQUEST['prenom'],$_REQUEST['commentaire'])) {
+    $id = $_REQUEST['id'];
+    $nom = $_REQUEST['nom'];
+    $prenom = $_REQUEST['prenom'];
+    $commentaire = $_REQUEST['commentaire'];
+}
+
+// verification de l'existance des valeures du form (si ca existe je prepare les variables pour la modif dans la BDD)
+if (isset($_REQUEST['id_auteur'],$_REQUEST['nom_auteur'],$_REQUEST['prenom_auteur'],$_REQUEST['commentaire_auteur'])) {
+    $id_auteur = $_REQUEST['id_auteur'];
+    $nom_auteur = $_REQUEST['nom_auteur'];
+    $prenom_auteur = $_REQUEST['prenom_auteur'];
+    $commentaire_auteur = $_REQUEST['commentaire_auteur'];
+
+    $sql = "UPDATE auteur SET nom_auteur = :nom_auteur, prenom_auteur = :prenom_auteur, commentaire = :commentaire WHERE numero_auteur = :numero_auteur";
+    $temp = $pdo->prepare($sql);
+    $temp->bindParam(':nom_auteur', $nom_auteur);
+    $temp->bindParam(':prenom_auteur', $prenom_auteur);
+    $temp->bindParam(':commentaire', $commentaire_auteur);
+    $temp->bindParam(':numero_auteur', $id_auteur);
+    $temp->execute();
+
+    header('Location: auteur_01.php');
+    exit();
+}
+?>
+<!doctype html>
+<html lang="fr">
+    <head>
+        <title>Projet - Biblio</title>
+        <meta charset="utf-8" />
+        <meta name="viewport"content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <link rel="stylesheet" href="../../CSS/css_bibliotheque.css">
+    </head>
+
+    <body>
+        <header>
+            <?php
+            // include("includes/header.php");
+            ?>
+        </header>
+        <main>
+            <div>
+                <form action="auteur_02.php">
+                    <h2>Modifier un auteur : </h2>
+
+                    <input type="hidden" name="id_auteur" value="<?=$id?>"/>
+
+                    <label for="nom_auteur">Nom :</label>
+                    <input type="text" name="nom_auteur" value="<?=$nom?>" required/>
+
+                    <label for="prenom_auteur">Prénom :</label>
+                    <input type="text" name="prenom_auteur" value="<?=$prenom?>" required/>
+
+                    <label for="commentaire_auteur">Commentaire :</label>
+                    <input type="text" name="commentaire_auteur" value="<?=$commentaire?>" required/>
+
+                    <input type="submit" value="Modifier" />
+                </form>
+            </div>
+
+
+        </main>
+        <footer>
+            <?php
+            // include("includes/footer.php");
+            ?>
+        </footer>
+    </body>
+</html>
