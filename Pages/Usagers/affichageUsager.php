498899b3615871f22d85680b8f19d1365252439d
Clément.Chevalier <clementchevalier2102@outlook.fr>
2024-04-08 14:37:43 +0200

    page affichage

--- Pages/Usagers/affichageUsager.php
+++ Pages/Usagers/affichageUsager.php
@@ -1,6 +1,17 @@
 <?php 
     require_once '../../includes/connexion.php';
     require_once '../../includes/navbar.php';
+
+    $sql = 'SELECT * FROM usager';
+    $temp = $pdo->query($sql);
+    while ($usager = $temp->fetch()){
+        $id = htmlentities($usager["id"]);
+        $nom = htmlentities($usager["nom_usager"]);
+        $prenom = htmlentities($usager["prenom_usager"]);
+        $ville = htmlentities($usager["ville_usager"]);
+        $bibliotheque = htmlentities($usager["numero_bibliotheque"]);
+        $commentaire = htmlentities($usager["commentaire"]);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +22,32 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <main>
+        <table>
+            <tr>
+                <th>Nom</th>
+                <th>Prenom</th>
+                <th>Ville</th>
+                <th>Numero bibliotheque</th>
+                <th>commentaire</th>
+                <th>Action</th>
+            </tr>
+            <tr>
+                <td><?= $nom ?></td>
+                <td><?= $prenom ?></td>
+                <td><?= $ville ?></td>
+                <td><?= $bibliotheque ?></td>
+                <td><?= $commentaire ?></td>
+                <td> 
+                    <a href="usager_02.php?id=<?= $id ?>">
+                        <img src="../../Medias/editform.png" class="boutonsform" alt="image de modification">
+                    </a>
+                    <a href="usager_03.php?id=<?= $id ?>">
+                        <img src="../../Medias/supprimerform.png" class="boutonsform" alt="image de suppresion">
+                    </a>
+                </td>
+            </tr>
+        </table>
+    </main>
 </body>
 </html>
