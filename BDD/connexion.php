c471d6866c356f02781c0ec4c779124363b985d3
Maillard <151616522+MaillardLucas@users.noreply.github.com>
2024-04-08 11:25:36 +0200

    create connexion.php

---
+++ BDD/connexion.php
@@ -0,0 +1,11 @@
+<?php
+$host = '127.0.0.1';
+$db = 'db_bibliotheque';
+$user = 'user_bibliotheque';
+$pass = 'mdp_bibliotheque';
+$port = 3306;
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+$pdo = new PDO($dsn, $user, $pass);
+?>
