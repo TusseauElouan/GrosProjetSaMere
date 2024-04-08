d1216950481d752c0fe923b50e9d926421cecc0d
Gabriel Delechamp <gabriel.delechamp.pro@gmail.com>
2024-04-08 11:10:30 +0200

    ajout navbar+ bibliotheque

---
+++ includes/navbar.php
@@ -0,0 +1,12 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Menu principal</a></li>
+        <li><a href="bibliotheque_01.php">Gestion des bibliothèques</a></li>
+        <li><a href="auteur_01.php">Gestion des auteurs</a></li>
+        <li><a href="ouvrage_01.php">Gestion des ouvrages</a></li>
+        <li><a href="usager_01.php">Gestion des usagers</a></li>
+        <li><a href="emprunt_01.php">Gestion des emprunts</a></li>
+        <li><a href="retour_01.php">Gestion des retours</a></li>
+        <li><a href="transfert_01.php">Gestion des transferts</a></li>
+    </ul>
+</nav>
