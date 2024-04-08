15d4963932bef5842f54411cbd84b19e2feb93d6
T.Elouan <46402839+TusseauElouan@users.noreply.github.com>
2024-04-08 11:04:52 +0200

    Formulaire

---
+++ Pages/Transferts/formulaire_ajout.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire d'ajout d'un transfert</title>
+</head>
+<body>
+    <form action="" method='POST'>
+        <label for=""></label>
+    </form>
+</body>
+</html>
