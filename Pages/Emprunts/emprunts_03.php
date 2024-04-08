dd7cc76497ea4ff5721ee305dc71de180a3114dc
Félis Maillard <151616377+FelisMaillard@users.noreply.github.com>
2024-04-08 16:30:34 +0200

    fix emprunt 01

--- Pages/Emprunts/emprunts_03.php
+++ Pages/Emprunts/emprunts_03.php
@@ -14,7 +14,7 @@ if (isset($_REQUEST['del'])) {
 <!doctype html>
 <html lang="fr">
     <head>
-        <title>Projet - Biblio</title>
+        <title>Suppression Auteur</title>
         <meta charset="utf-8" />
         <meta name="viewport"content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <link rel="stylesheet" href="../../CSS/css_bibliotheque.css">
