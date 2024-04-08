710ec1d88591bef8a4e0c55af021f1eb81e97c85
Clément.Chevalier <clementchevalier2102@outlook.fr>
2024-04-08 10:27:49 +0200

    Update usagers_01.php

---
+++ Pages/usagers/usagers_01.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="affichageUsager.php" method="post">
+        <label for="nom">Nom</label>
+        <input type="text" name='nom'>
+        <label for="prenom">prenom</label>
+        <input type="text" name="prenom">
+        <label for="ville">Ville</label>
+        <input type="text" name="ville">
+        <label for="biblioteque"></label>
+        <input type="text" name="biblioteque">
+        <label for="commentaire">commentaire</label>
+        <input type="text" name="commentaire" id="commentaire">
+    </form>
+</body>
+</html>
