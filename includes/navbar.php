e2a6849c2b0636e3a655f139684e01d151cd6f2e
Theo Foucher <theonicolas.foucher@gmail.com>
2024-04-12 15:38:41 +0200

    Modification tailles textes + tailles tableaux

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,10 @@
 <nav>
     <a href="../../index.php">Menu principal</a>
-    <a href="../Bibliotheques/bibliotheque_01.php">Gestion des bibliothèques</a>
-    <a href="../Auteurs/auteur_01.php">Gestion des auteurs</a>
-    <a href="../Ouvrages/ouvrages_01.php">Gestion des ouvrages</a>
-    <a href="../Usagers/usagers_01.php">Gestion des usagers</a>
-    <a href="../Emprunts/emprunt_01.php">Gestion des emprunts</a>
-    <a href="../Retours/retour_01.php">Gestion des retours</a>
-    <a href="../Transferts/transfert_01.php">Gestion des transferts</a>
+    <a href="../Bibliotheques/bibliotheque_01.php">Bibliothèques</a>
+    <a href="../Auteurs/auteur_01.php">Auteurs</a>
+    <a href="../Ouvrages/ouvrages_01.php">Ouvrages</a>
+    <a href="../Usagers/usagers_01.php">Usagers</a>
+    <a href="../Emprunts/emprunt_01.php">Emprunts</a>
+    <a href="../Retours/retour_01.php">Retours</a>
+    <a href="../Transferts/transfert_01.php">Transferts</a>
 </nav>
